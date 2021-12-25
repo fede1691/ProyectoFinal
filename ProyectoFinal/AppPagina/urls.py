0bf20837d7b112fbe5e7d00543103dd6aa5c80ae
Federico Gomez <g.federico1691@gmail.com>
2021-12-25 19:38:11 -0300

    Ajustes en templates y cambio de nombre en vistas y urls

--- ProyectoFinal/AppPagina/urls.py
+++ ProyectoFinal/AppPagina/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('saludo', views.saludo, name="Saludo"),
 
     ##FORMULARIOS
-    path('cursoFormulario', views.cursoFormulario, name="CursoFormulario"),
-    path('alumnoFormulario', views.alumnoFormulario, name="AlumnoFormulario"),
-    path('maestroFormulario', views.maestroFormulario, name="MaestroFormulario"),
     path('busquedaAlumno', views.busquedaAlumno, name="BusquedaAlumno"),
     path('buscar/', views.buscar, name="Buscar"),
 
@@ -17,17 +14,17 @@ urlpatterns = [
     path('leerCursos', views.leerCursos, name="LeerCursos"),
     path('eliminarCursos/<curso_nombre>', views.eliminarCursos, name="EliminarCursos"),
     path('editarCursos/<curso_nombre>', views.editarCursos, name="EditarCursos"),
-    path('curso/lista', views.ListaCursos.as_view(), name="List"),
-    path(r'^(?P<pk>\d+)$', views.DetalleCursos.as_view(), name="Detail"),
-    path(r'^nuevo$', views.CreacionCursos.as_view(), name="New"),
-    path(r'^editar/(?P<pk>\d+)$', views.ActualizaCursos.as_view(), name="Edit"),
-    path(r'^borrar/(?P<pk>\d+)$', views.BorrarCursos.as_view(), name="Delete"),
+    path('curso/lista', views.ListaCursos.as_view(), name="ListaCursos"),
+    path(r'^(?P<pk>\d+)$', views.DetalleCursos.as_view(), name="DetalleCursos"),
+    path(r'^nuevo$', views.CrearCurso.as_view(), name="CrearCurso"),
+    path(r'^editar/(?P<pk>\d+)$', views.EditarCursos.as_view(), name="EditarCursos"),
+    path(r'^borrar/(?P<pk>\d+)$', views.BorrarCursos.as_view(), name="BorrarCursos"),
 
     ##ALUMNOS
     
-    path('alumno/lista', views.ListaAlumno.as_view(), name="ListAlumno"),
+    path('alumno/lista', views.ListaAlumnos.as_view(), name="ListaAlumnos"),
     path(r'^detalleAlumno/(?P<pk>\d+)$', views.DetalleAlumno.as_view(), name="DetailAlumno"),
-    path(r'^nuevoAlumno$', views.NuevoAlumno.as_view(), name="NewAlumno"),
+    path(r'^nuevoAlumno$', views.CrearAlumno.as_view(), name="CrearAlumno"),
     path(r'^editarAlumno/(?P<pk>\d+)$', views.ActualizaAlumno.as_view(), name="EditAlumno"),
     path(r'^borrarAlumno/(?P<pk>\d+)$', views.BorrarAlumno.as_view(), name="DeleteAlumno"),
     
--- ProyectoFinal/AppPagina/views.py
+++ ProyectoFinal/AppPagina/views.py
@@ -18,76 +18,6 @@ def saludo(request):
 
     return render(request, "AppPagina/saludo.html")
 
-def cursoFormulario(request):
-    
-    if request.method == 'POST':
-
-        miFormulario = CursoFormulario(request.POST)
-
-        if miFormulario.is_valid():
-
-            informacion = miFormulario.cleaned_data
-
-            curso = Curso(nombre = informacion['nombre'], comision = informacion['comision'])
-
-            curso.save()
-
-            return render(request, "AppPagina/cursoFormulario")
-    else:
-
-        miFormulario = CursoFormulario()
-
-    return render(request, "AppPagina/cursoFormulario", {"miFormulario":miFormulario})
-
-def alumnoFormulario(request):
-    
-    if request.method == 'POST':
-
-        formAlumno = AlumnoFormulario(request.POST)
-
-        if formAlumno.is_valid():
-
-            informacion = formAlumno.cleaned_data
-
-            alumno = Alumno(nombre = informacion['nombre'], 
-                            apellidos = informacion['apellidos'], 
-                            nacimiento = informacion['nacimiento'], 
-                            sexo = informacion['sexo'])
-
-            alumno.save()
-
-            return render(request, "AppPagina/inicio.html")
-    else:
-
-        formAlumno = AlumnoFormulario()
-
-    return render(request, "AppPagina/alumnoFormulario", {"formAlumno":formAlumno})
-
-def maestroFormulario(request):
-    
-    if request.method == 'POST':
-
-        formMaestro = MaestroFormulario(request.POST)
-
-        if formMaestro.is_valid():
-
-            informacion = formMaestro.cleaned_data
-
-            maestro = Maestro(nombre = informacion['nombre'], 
-                              apellidos = informacion['apellidos'], 
-                              nacimiento = informacion['nacimiento'], 
-                              sexo = informacion['sexo'],
-                              titulo = informacion['titulo'])
-
-            maestro.save()
-
-            return render(request, "AppPagina/inicio.html")
-    else:
-
-        formMaestro = MaestroFormulario()
-
-    return render(request, "AppPagina/maestroFormulario", {"formMaestro":formMaestro})
-
 ########################### FUNCIONES PARA LEER MODELS#############################################
 
 def leerCursos(request):
@@ -171,18 +101,18 @@ def buscar(request):
 ######################## CBVs CURSO ##################################
 class ListaCursos(ListView):
     model = Curso
-    template_name = "AppPagina/curso_list"
+    template_name = "AppPagina/lista_cursos"
 
 class DetalleCursos(DetailView):
     model = Curso
-    template_name = "AppPagina/curso_detalle"
+    template_name = "AppPagina/detalle_cursos"
     
-class CreacionCursos(CreateView):
+class CrearCurso(CreateView):
     model = Curso
     success_url = "/AppPagina/curso/lista"
     fields = ['nombre', 'comision']
 
-class ActualizaCursos(UpdateView):
+class EditarCursos(UpdateView):
     model = Curso
     success_url = "/AppPagina/curso/lista"
     fields = ['nombre', 'comision']
@@ -192,15 +122,15 @@ class BorrarCursos(DeleteView):
     success_url = "/AppPagina/curso/lista"
 
 ######################## CBVs ALUMNO ##################################
-class ListaAlumno(ListView):
+class ListaAlumnos(ListView):
     model = Alumno
-    template_name = "AppPagina/alumno_list"
+    template_name = "AppPagina/lista_alumnos"
 
 class DetalleAlumno(DetailView):
     model = Alumno
     template_name = "AppPagina/alumno_detalle"
     
-class NuevoAlumno(CreateView):
+class CrearAlumno(CreateView):
     model = Alumno
     success_url = "/AppPagina/alumno/lista"
     fields = ['nombre', 'apellidos', 'nacimiento', 'sexo']
