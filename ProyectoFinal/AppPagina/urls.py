9dfb58d07e92a8e47a9a401073d5bbf7e44afecb
Federico Gomez <g.federico1691@gmail.com>
2021-12-30 09:09:50 -0300

    Cambios en detalles de templates

--- ProyectoFinal/AppPagina/urls.py
+++ ProyectoFinal/AppPagina/urls.py
@@ -11,9 +11,7 @@ urlpatterns = [
 
 
     ##CURSOS
-    path('leerCursos', views.leerCursos, name="LeerCursos"),
-    path('eliminarCursos/<curso_nombre>', views.eliminarCursos, name="EliminarCursos"),
-    path('editarCursos/<curso_nombre>', views.editarCursos, name="EditarCursos"),
+     
     path('curso/lista', views.ListaCursos.as_view(), name="ListaCursos"),
     path(r'^(?P<pk>\d+)$', views.DetalleCursos.as_view(), name="DetalleCursos"),
     path(r'^nuevo$', views.CrearCurso.as_view(), name="CrearCurso"),
@@ -23,14 +21,14 @@ urlpatterns = [
     ##ALUMNOS
     
     path('alumno/lista', views.ListaAlumnos.as_view(), name="ListaAlumnos"),
-    path(r'^detalleAlumno/(?P<pk>\d+)$', views.DetalleAlumno.as_view(), name="DetailAlumno"),
+    path(r'^detalleAlumno/(?P<pk>\d+)$', views.DetalleAlumno.as_view(), name="DetalleAlumno"),
     path(r'^nuevoAlumno$', views.CrearAlumno.as_view(), name="CrearAlumno"),
-    path(r'^editarAlumno/(?P<pk>\d+)$', views.ActualizaAlumno.as_view(), name="EditAlumno"),
-    path(r'^borrarAlumno/(?P<pk>\d+)$', views.BorrarAlumno.as_view(), name="DeleteAlumno"),
+    path(r'^editarAlumno/(?P<pk>\d+)$', views.ActualizaAlumno.as_view(), name="EditarAlumno"),
+    path(r'^borrarAlumno/(?P<pk>\d+)$', views.BorrarAlumno.as_view(), name="BorrarAlumno"),
     
 
-    ##Maestros
-    path('leerMaestros', views.leerMaestros, name="LeerMaestros"),
+    ##MAESTROS
+    
     path('maestro/lista', views.ListaMaestro.as_view(), name="ListMaestro"),
     path(r'^detalleMaestro/(?P<pk>\d+)$', views.DetalleMaestro.as_view(), name="DetailMaestro"),
     path(r'^nuevoMaestro$', views.NuevoMaestro.as_view(), name="NewMaestro"),
